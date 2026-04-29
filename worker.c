15a215cc2d0dc7d826a495ae3e817892c97bc132
kirill slabun <slabun@opsys.cs.umsl.edu>
2026-04-29 14:09:18 -0500

    Reorganized the worker code to enhance readability and incorporated comprehensive comments. Implemented additional functionality to display worker information upon both initialization and termination.

--- worker.c
+++ worker.c
@@ -8,7 +8,11 @@
 #include <time.h>
 #include "shared.h"
 
+int shm_key;
+int shm_id;
+
 int main(int argc, char *argv[]) {
+    // Check whether the right number of arguments were passed.
     if (argc < 2) {
         fprintf(stderr, "Usage: worker t\n");
         exit(1);
@@ -17,14 +21,39 @@ int main(int argc, char *argv[]) {
     double t_limit = atof(argv[1]);
     srand(time(NULL) ^ (getpid() << 16));
 
-    key_t shm_key = ftok("oss.c", 'R');
-    int shm_id = shmget(shm_key, sizeof(int) * 2, PERMS);
+    // Shared memory
+    shm_key = ftok("oss.c", 'R');
+    if (shm_key <= 0 ) {
+        fprintf(stderr,"Child:... Error in ftok\n");
+        exit(1);
+    }
+    
+    // Create shared memory segment
+    shm_id = shmget(shm_key, sizeof(int)*2, PERMS);
+    if (shm_id <= 0 ) {
+        fprintf(stderr,"child:... Error in shmget\n");
+        exit(1);
+    }
+
+    // Attach to shared memory
     int *clockptr = (int *)shmat(shm_id, NULL, 0);
     int *sec = &clockptr[0];
     int *nano = &clockptr[1];
 
-    key_t msg_key = ftok("msgq.txt", 1);
-    int msqid = msgget(msg_key, PERMS);
+    // Get a key for our message queue
+    key_t msg_key;
+    if ((msg_key = ftok("msgq.txt", 1)) == -1) {
+        perror("ftok");
+        exit(1);
+    }
+    // Create message queue
+    int msqid = 0;    
+    if ((msqid = msgget(msg_key, PERMS)) == -1) {
+        perror("msgget in child");
+        exit(1);
+    }
+
+    printf("Worker %d has access to the queue \n", getpid());
 
     int resources_held[NUM_RESOURCES] = {0};
     
@@ -35,6 +64,10 @@ int main(int argc, char *argv[]) {
     int term_nano = *nano + run_nano;
     if (term_nano >= 1000000000) { term_sec++; term_nano -= 1000000000; }
 
+    printf("WORKER PID:%d PPID:%d\n", getpid(), getppid());
+    printf("SysClockS:%d SysClockNano:%d TermTimeS:%d TermTimeNano:%d\n", *sec, *nano, term_sec, term_nano);
+    printf("--Just Starting\n");
+
     msgbuffer msg;
 
     while (1) {
@@ -43,6 +76,9 @@ int main(int argc, char *argv[]) {
 
         // Check if time to terminate
         if (*sec > term_sec || (*sec == term_sec && *nano >= term_nano)) {
+            printf("WORKER PID:%d PPID:%d\n", getpid(), getppid());
+            printf("SysClockS:%d SysClockNano:%d TermTimeS:%d TermTimeNano:%d\n", *sec, *nano, term_sec, term_nano);
+            printf("--Terminating\n");
             msg.mtype = 1;
             msg.intData = 0; // Terminate
             msgsnd(msqid, &msg, sizeof(msgbuffer) - sizeof(long), 0);
@@ -79,6 +115,9 @@ int main(int argc, char *argv[]) {
                     resources_held[found]++;
                 } else {
                     // Holding all resources? Terminate.
+                    printf("WORKER PID:%d PPID:%d\n", getpid(), getppid());
+                    printf("SysClockS:%d SysClockNano:%d TermTimeS:%d TermTimeNano:%d\n", *sec, *nano, term_sec, term_nano);
+                    printf("--Terminating because holding all resources\n");
                     msg.mtype = 1;
                     msg.intData = 0;
                     msgsnd(msqid, &msg, sizeof(msgbuffer) - sizeof(long), 0);
