186fb5802bc613c032f5b0afb73030a9c5f012c1
kirill slabun <slabun@opsys.cs.umsl.edu>
2026-04-29 01:03:24 -0500

    Organized OSS, improved readability, and added comments. Changed how information is displayed and logged to a file.

--- oss.c
+++ oss.c
@@ -9,14 +9,17 @@
 #include <string.h>
 #include <time.h>
 #include <errno.h>
+#include <stdarg.h>
 #include <sys/msg.h>
 #include "shared.h"
 
 // Global variables for signal cleanup
 const int BUFF_SIZE = sizeof(int) * 2;
+int shm_key;
 int shm_id;
 int *clockptr;
 int msqid;
+FILE *log_file = NULL;
 
 void cleanup(int sig) {
     if (sig != 0) {
@@ -26,12 +29,32 @@ void cleanup(int sig) {
         kill(0, SIGTERM);
     }
 
+    // Detach and remove shared memory
     if (clockptr != NULL)
         shmdt(clockptr);
+    
+    // Remove shared memory and message queue
     shmctl(shm_id, IPC_RMID, NULL);
     msgctl(msqid, IPC_RMID, NULL);
 
+    // Remove the temporary file used for ftok
+    unlink("msgq.txt");
+
     exit(sig == 0 ? 0 : 1);
+}
+
+void log_msg(const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    vprintf(fmt, args);
+    va_end(args);
+
+    if (log_file) {
+        va_start(args, fmt);
+        vfprintf(log_file, fmt, args);
+        va_end(args);
+        fflush(log_file);
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -39,52 +62,129 @@ int main(int argc, char *argv[]) {
     int opt;
     int n = -1, s = -1;
     double t = -1, i = -1;
-    FILE *f = stdout;
+    FILE *f = NULL; // Log file pointer
 
+    // Parse command line arguments
     while ((opt = getopt(argc, argv, "hn:s:t:i:f:")) != -1) {
         switch (opt) {
             case 'h':
-                printf("Usage: %s [-h] [-n proc] [-s simul] [-t timelimit] [-i interval] [-f logfile]\n", argv[0]);
+                printf("Usage: %s [-h] [-n proc] [-s simul] [-t timelimitForChildren] [-i intervalInSecondsToLaunchChildren] [-f logfile]\n", argv[0]);
+                printf("  -h Display help message and exit\n");
+                printf("  -n proc   Total number of child processes to launch\n");
+                printf("  -s simul  Maximum number of children running simultaneously\n");
+				printf("  -t iter   Upper bound of simulated time each child runs\n");
+                printf("  -i sec    Interval in simulated seconds to launch new children\n");
+                printf("  -f file   Log output to specified file\n");
                 return 0;
-            case 'n': n = atoi(optarg); break;
-            case 's': s = atoi(optarg); break;
-            case 't': t = atof(optarg); break;
-            case 'i': i = atof(optarg); break;
-            case 'f': f = fopen(optarg, "w"); break;
+            case 'n':
+                n = atoi(optarg);
+                break;
+            case 's':
+                s = atoi(optarg);
+                break;
+            case 't':
+                t = atof(optarg);
+                break;
+            case 'i':
+                i = atof(optarg);
+                break;
+            case 'f':
+                // log file
+                f = fopen(optarg, "w");
+                if (!f) {
+                    perror("OSS: Failed to open log file");
+                    return 1;
+                }
+                log_file = f;
+                break;
+            default:
+                printf("Usage: %s [-h] [-n proc] [-s simul] [-t timelimitForChildren] [-i intervalInSecondsToLaunchChildren] [-f logfile]\n", argv[0]);
+                printf("  -h Display help message and exit\n");
+                printf("  -n proc   Total number of child processes to launch\n");
+				printf("  -s simul  Maximum number of children running simultaneously\n");
+				printf("  -t iter   Upper bound of simulated time each child runs\n");
+                printf("  -i sec    Interval in simulated seconds to launch new children\n");
+                printf("  -f file   Log output to specified file\n");
+                return 1;
         }
     }
 
     if (n <= 0 || s <= 0 || t <= 0 || i < 0) {
-        fprintf(stderr, "Invalid arguments\n");
+        fprintf(stderr, "Missing or invalid required arguments\n");
+        printf("Usage: %s [-h] [-n proc] [-s simul] [-t timelimitForChildren] [-i intervalInSecondsToLaunchChildren]\n", argv[0]);
+        printf("-n value must be greater then 0. \n");
+        printf("-s value must be greater then 0. \n");
+        printf("-t value must be greater then 0. \n");
+        printf("-i value must be greater or equal to 0. \n");
         return 1;
     }
 
+    // Print initial configuration
+    log_msg("OSS: OSS starting, PID:%d PPID:%d\n", getpid(), getppid());
+    log_msg("OSS: Called with:\n-n %d\n-s %d\n-t %.3f\n-i %.3f\n\n", n, s, t, i);
+
+    // Listen for SIGINT and SIGALRM to trigger cleanup (alarm after 60s to prevent infinite runs)
     signal(SIGINT, cleanup);
     signal(SIGALRM, cleanup);
     alarm(60);
+    
+    // Create shared memory
+    shm_key = ftok("oss.c", 'R'); // Generate a unique key for shared memory
+    if (shm_key <= 0) {
+        fprintf(stderr, "OSS: Failed to generate shared memory key (ftok failed)\n");
+        return 1;
+    }
 
-    key_t shm_key = ftok("oss.c", 'R');
-    shm_id = shmget(shm_key, BUFF_SIZE, PERMS | IPC_CREAT);
-    clockptr = (int *)shmat(shm_id, NULL, 0);
+    // Create shared memory segment
+    shm_id = shmget(shm_key, BUFF_SIZE, PERMS|IPC_CREAT); 
+    if (shm_id <= 0) {
+        fprintf(stderr, "OSS: Failed to create shared memory segment (shmget failed)\n");
+        return 1;
+    }
+
+    // Attach to shared memory
+    clockptr = (int *)shmat(shm_id, NULL, 0); 
+    if (clockptr == (void *) -1) {
+        fprintf(stderr, "OSS: Failed to attach to shared memory (shmat failed)\n");
+        return 1;
+    }
+
+    // Initialize shared clock
     int *sec = &clockptr[0];
     int *nano = &clockptr[1];
     *sec = 0; *nano = 0;
 
-    key_t msg_key = ftok("msgq.txt", 1);
-    if (msg_key == -1) {
-        FILE *tmp = fopen("msgq.txt", "w");
-        fclose(tmp);
-        msg_key = ftok("msgq.txt", 1);
+    // Create a file for ftok to use for message queue key generation
+    FILE *tmp = fopen("msgq.txt", "w");
+    if (tmp == NULL) {
+        perror("OSS: fopen msgq.txt");
+        exit(1);
     }
-    msqid = msgget(msg_key, PERMS | IPC_CREAT);
+    fclose(tmp);
 
+    // Get a key for our message queue
+    key_t msg_key;
+    if ((msg_key = ftok("msgq.txt", 1)) == -1) {
+        perror("OSS: ftok");
+        exit(1);
+    }
+
+    // Create message queue
+    if ((msqid = msgget(msg_key, PERMS | IPC_CREAT)) == -1) {
+        perror("OSS: msgget in parent");
+        exit(1);
+    }
+
+    log_msg("OSS: Message queue created with ID: %d\n", msqid);
+
+    // Initialize PCB table, resource availability, and request tracking
     struct PCB table[MAX_PCB];
     memset(table, 0, sizeof(table));
-    for(int j=0; j<MAX_PCB; j++) table[j].requested_resource = -1;
-
+    for(int j=0; j<MAX_PCB; j++) table[j].requested_resource = -1; // -1 means no request
     int resource_available[NUM_RESOURCES];
     for (int j = 0; j < NUM_RESOURCES; j++) resource_available[j] = INSTANCES_PER_RESOURCE;
 
+    // Main loop variables
     int running = 0, total_launched = 0, total_requests = 0, granted_immediately = 0;
     int last_launch_sec = 0, last_launch_nano = 0;
     int last_print_sec = -1, last_print_nano = -1;
@@ -105,7 +205,7 @@ int main(int argc, char *argv[]) {
                     table[j].resources_allocated[res]++;
                     table[j].blocked = 0;
                     table[j].requested_resource = -1;
-                    fprintf(f, "Master granting P%d blocked request for R%d at time %d:%d\n", j, res, *sec, *nano);
+                    log_msg("OSS: Master granting P%d blocked request for R%d at time %d:%d\n", j, res, *sec, *nano);
                     msg.mtype = table[j].pid;
                     msg.intData = 1; // Granted
                     msgsnd(msqid, &msg, sizeof(msgbuffer) - sizeof(long), 0);
@@ -131,7 +231,7 @@ int main(int argc, char *argv[]) {
                 table[index].start_sec = *sec; table[index].start_nanosec = *nano;
                 running++; total_launched++;
                 last_launch_sec = *sec; last_launch_nano = *nano;
-                fprintf(f, "Master launching P%d at time %d:%d\n", index, *sec, *nano);
+                log_msg("OSS: Master launching P%d at time %d:%d\n", index, *sec, *nano);
             }
         }
 
@@ -149,7 +249,7 @@ int main(int argc, char *argv[]) {
 
             msgrcv(msqid, &msg, sizeof(msgbuffer) - sizeof(long), 1, 0);
             if (msg.intData == 0) { // Terminating
-                fprintf(f, "Master has acknowledged Process P%d terminating at time %d:%d\n", found, *sec, *nano);
+                log_msg("OSS: Master has acknowledged Process P%d terminating at time %d:%d\n", found, *sec, *nano);
                 for (int j = 0; j < NUM_RESOURCES; j++) {
                     resource_available[j] += table[found].resources_allocated[j];
                     table[found].resources_allocated[j] = 0;
@@ -160,23 +260,23 @@ int main(int argc, char *argv[]) {
             } else if (msg.intData > 0) { // Request
                 int res = msg.intData - 1;
                 total_requests++;
-                fprintf(f, "Master has detected Process P%d requesting R%d at time %d:%d\n", found, res, *sec, *nano);
+                log_msg("OSS: Master has detected Process P%d requesting R%d at time %d:%d\n", found, res, *sec, *nano);
                 if (resource_available[res] > 0) {
                     resource_available[res]--;
                     table[found].resources_allocated[res]++;
                     granted_immediately++;
-                    fprintf(f, "Master granting P%d request R%d at time %d:%d\n", found, res, *sec, *nano);
+                    log_msg("OSS: Master granting P%d request R%d at time %d:%d\n", found, res, *sec, *nano);
                     msg.mtype = table[found].pid;
                     msg.intData = 1; // Granted
                     msgsnd(msqid, &msg, sizeof(msgbuffer) - sizeof(long), 0);
                 } else {
                     table[found].blocked = 1;
                     table[found].requested_resource = res;
-                    fprintf(f, "Master blocking P%d for R%d at time %d:%d\n", found, res, *sec, *nano);
+                    log_msg("OSS: Master blocking P%d for R%d at time %d:%d\n", found, res, *sec, *nano);
                 }
             } else if (msg.intData < 0) { // Release
                 int res = abs(msg.intData) - 1;
-                fprintf(f, "Master has acknowledged Process P%d releasing R%d at time %d:%d\n", found, res, *sec, *nano);
+                log_msg("OSS: Master has detected Process P%d releasing R%d at time %d:%d\n", found, res, *sec, *nano);
                 resource_available[res]++;
                 table[found].resources_allocated[res]--;
                 msg.mtype = table[found].pid;
@@ -188,23 +288,24 @@ int main(int argc, char *argv[]) {
         // Print tables every 0.5s
         if (*sec > last_print_sec || (*sec == last_print_sec && *nano >= last_print_nano + 500000000)) {
             last_print_sec = *sec; last_print_nano = *nano;
-            fprintf(f, "\nOSS PID:%d SysClockS:%d SysClockNano:%d\n", getpid(), *sec, *nano);
-            fprintf(f, "Resources available:\n");
-            for(int j=0; j<NUM_RESOURCES; j++) fprintf(f, "R%d:%d ", j, resource_available[j]);
-            fprintf(f, "\nResources allocated:\n     ");
-            for(int j=0; j<NUM_RESOURCES; j++) fprintf(f, "R%2d ", j);
-            fprintf(f, "\n");
+
+            log_msg("\nOSS: OSS PID:%d SysClockS:%d SysClockNano:%d\n", getpid(), *sec, *nano);
+            log_msg("OSS: Resources available:\n");
+            for(int j=0; j<NUM_RESOURCES; j++) log_msg("R%d:%d ", j, resource_available[j]);
+            log_msg("\nOSS: Resources allocated:\n     ");
+            for(int j=0; j<NUM_RESOURCES; j++) log_msg("R%2d ", j);
+            log_msg("\n");
             for(int i=0; i<MAX_PCB; i++) {
                 if(table[i].occupied) {
-                    fprintf(f, "P%2d: ", i);
-                    for(int j=0; j<NUM_RESOURCES; j++) fprintf(f, "%3d ", table[i].resources_allocated[j]);
-                    fprintf(f, "\n");
+                    log_msg("P%2d: ", i);
+                    for(int j=0; j<NUM_RESOURCES; j++) log_msg("%3d ", table[i].resources_allocated[j]);
+                    log_msg("\n");
                 }
             }
         }
     }
 
-    fprintf(f, "\nFinal Report:\nTotal requests: %d\nGranted immediately: %d (%.2f%%)\n", total_requests, granted_immediately, total_requests > 0 ? (float)granted_immediately/total_requests*100 : 0);
+    log_msg("\nOSS: Final Report:\nTotal requests: %d\nGranted immediately: %d (%.2f%%)\n", total_requests, granted_immediately, total_requests > 0 ? (float)granted_immediately/total_requests*100 : 0);
     cleanup(0);
     return 0;
 }
